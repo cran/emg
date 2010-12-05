a6d799ed614ccdff14c12f5e68727c4c18e2bfa8
Shawn Garbett <Shawn.Garbett@Vanderbilt.edu>
2010-12-05 00:00:00 +0000

    version 1.0.3

--- R/pemg.R
+++ R/pemg.R
@@ -1,9 +1,12 @@
 # This analytical form was contributed by Mark Kozdoba
-# N(mu, sigma^2) + Exp(lambda)
-pemg <- function(q, mu=0, sigma=1, lambda=1)
+# N(mu,sigma^2) + Exp(lambda)
+pemg <- function (q, mu = 0, sigma = 1, lambda = 1)
 {
-  u <- (q - mu)*lambda
-  sigma1 <- lambda*sigma
+    #renormalize
+    u <- (q - mu) * lambda
+    sigma1 <- lambda * sigma
 
-  pnorm(u, 0, sigma) - exp(-u + (sigma1*sigma1)/2 + log(pnorm(u, sigma1*sigma1, sigma1)))
+    #compute for the case where mu=0, sigma = sigma1, lambda = 1
+    pnorm(u, 0, sigma1) - exp(-u + (sigma1 * sigma1)/2 + log(pnorm(u,
+        sigma1 * sigma1, sigma1)))
 }
